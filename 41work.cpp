fcf3893c7b13ec9c4fae31f70a27e1ea9b522774
Viktoria Poliakova <viktoria.v.poliakova@gmail.com>
2022-09-27 13:00:58 +0300

    End task

--- 41work.cpp
+++ 41work.cpp
@@ -2,20 +2,28 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+
 using namespace std;
 int main(){
-    int i, sumi, a;
-    string fileName, numString;
+    int i, sumi, a, sum, num;
+    string fileName, line;
     fileName="numbers.txt";
     ofstream fif(fileName);
     for (i=0; i<=9; i++){
         // cout<<"ВВедите"<<i<<"число"<<endl;
         // cin>>a;
-        fif<<rand()<<" ";
+        fif<<rand()<<endl;
     }
     fif.close();
-    ifstream fuf(fileName);
-    getline(fuf, numString);
-    cout<<numString;
-    fuf.close();
+
+    sum=0;    
+    ifstream fof(fileName);
+    while (getline(fof, line)){
+      cout<<line<<endl;  
+      sum+=stoi(line);
+    };
+
+    cout<<"Итоговая сумма:"<<sum<<endl;
+    
+    fof.close();
 }
